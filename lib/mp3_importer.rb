a05a7dfdc2c563200aa00000023e40bb9d0687f6
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-22 18:58:11 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,18 +6,19 @@ attr_accessor :path , :name
     @@all = []
   end
   def import
-  Song.new_by_filename(@path)
+    
+  fileSong.new_by_filename(@path)
 
   end
   def files
-file_directory =  Dir.entries(@path)
+@file_directory =  Dir.entries(@path)
 
-file_directory.each do |filename|
+@file_directory.each do |filename|
    artist_array=filename.split("-")
 
 end
-file_directory.delete(".")
-file_directory.delete("..")
+@file_directory.delete(".")
+@file_directory.delete("..")
 file_directory
   end
 end
