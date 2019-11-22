ad89936d9794dab7692d5d709f25990f1255bbfe
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-22 19:03:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
   def self.new_by_filename(file)
   file_array =  file.split("-")
- file_array[1]= Song.new(file_array[1])
+ file_array[1]= Song.new(file_array[1].strip)
   end
 
 end
